eb08234fb4d87000119191cdafd73ca2963aa817
Alterus <42892150+centellare@users.noreply.github.com>
2026-02-01 23:23:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true
+  port: 3000,
+  host: true,
+  allowedHosts: ['etherizesmartcrmv2-production.up.railway.app']
   },
   build: {
     outDir: 'dist',
